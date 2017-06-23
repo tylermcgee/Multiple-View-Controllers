42b23d14b74121ea1dc0506b39c01c9b86a6ed61
Tyler McGee <tmcgee2481@gmail.com>
2017-06-23 09:24:10 -0400

    Committed 6/23/17.

---
+++ Multiple View Controllers/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  Multiple View Controllers
+//
+//  Created by Syngenta RDIS Mac on 5/11/17.
+//  Copyright © 2017 Syngenta GenEx. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("second view controller loaded")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
